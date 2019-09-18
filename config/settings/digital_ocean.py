5b8bc900a07748a2f79ad139691650c524e6010d
JacobSpectorMD <JacobDSpector@gmail.com>
2019-09-18 08:39:10 -0500

    Added error logging admin emails

--- config/settings/digital_ocean.py
+++ config/settings/digital_ocean.py
@@ -160,3 +160,39 @@ AXES_LOCKOUT_TEMPLATE = 'account_locked.html'
 AXES_USERNAME_FORM_FIELD = 'email'
 AXES_ONLY_USER_FAILURES = False
 AXES_RESET_ON_SUCCESS = True
+
+ADMINS = [('Jacob', 'clinicaltad@gmail.com')]
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s [%(asctime)s] %(module)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'file': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'verbose',
+            'filename': '/var/www/logs/ibiddjango.log',
+            'maxBytes': 1024000,
+            'backupCount': 3,
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file', 'console', 'mail_admins'],
+            'propagate': True,
+            'level': 'DEBUG',
+        },
+    }
+}
