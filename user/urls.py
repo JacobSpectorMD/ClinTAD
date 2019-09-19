42eca40276bb59f19912b7419973f3ff88c792da
JacobSpectorMD <JacobDSpector@gmail.com>
2019-09-19 07:19:17 -0400

    Working on error logging

--- user/urls.py
+++ user/urls.py
@@ -19,12 +19,12 @@ urlpatterns = [
     url(r'^default_cnvs/$', default_cnvs, name='default_cnvs'),
 
     url(r'^password_reset/$', auth_views.PasswordResetView.as_view(template_name='password_reset_form.html'),
-        name='password_reset'),
+        {'extra_context':{'navbar':'reset'}}, name='password_reset'),
     url(r'^password_reset/done/$', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'),
-        name='password_reset_done'),
+        {'extra_context':{'navbar':'reset'}}, name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),
-        name='password_reset_confirm'),
+        {'extra_context':{'navbar':'reset'}}, name='password_reset_confirm'),
     url(r'^reset/done/$', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
-        name='password_reset_complete'),
+        {'extra_context':{'navbar':'reset'}}, name='password_reset_complete'),
 ]
