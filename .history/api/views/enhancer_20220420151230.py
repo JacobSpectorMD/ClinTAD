0f3058bf306954c88851da7533de083926cbe2be
marsof02 <66377681+marsof02@users.noreply.github.com>
2022-08-30 14:08:08 -0400

    feat: push extra changes

--- .history/api/views/enhancer_20220420151230.py
+++ .history/api/views/enhancer_20220420151230.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 from home.models import Enhancer
 from api.serializers import EnhancerSerializer
 
-# Needs track info
+
 class EnhancerViewSet(viewsets.ModelViewSet):
     serializer_class = EnhancerSerializer
     queryset = Enhancer.objects.all()
