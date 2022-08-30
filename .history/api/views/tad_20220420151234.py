7385c7641e506fd20f39864ffb7bc77b40e43d88
marsof02 <66377681+marsof02@users.noreply.github.com>
2022-08-30 14:08:08 -0400

    feat: push extra changes

--- .history/api/views/tad_20220420151234.py
+++ .history/api/views/tad_20220420151234.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 from home.models import TAD
 from api.serializers import TADSerializer
 
-#NO
+
 class TADViewSet(viewsets.ModelViewSet):
     serializer_class = TADSerializer
     queryset = TAD.objects.all()
--- .history/api/views/ut_20220422181034.py
+++ .history/api/views/ut_20220422181034.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 from home.models import UT
 from api.serializers import UTSerializer
 
-#NO -- probably needs authentication first
+
 class UTViewSet(viewsets.ModelViewSet):
     serializer_class = UTSerializer
     queryset = UT.objects.all()
